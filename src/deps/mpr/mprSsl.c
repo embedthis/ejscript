fb85a51d503fc376c07dc24e8550508fff95398c
Embedthis Software <dev@embedthis.com>
2010-05-23 19:14:04 -0700

    Update deps

--- src/deps/mpr/mprSsl.c
+++ src/deps/mpr/mprSsl.c
@@ -6503,6 +6503,7 @@ typedef struct Mpr {
     int             hasDedicatedService;    /**< Running a dedicated events thread */
     int             allocPolicy;            /**< Memory allocation depletion policy */
     int             logFd;                  /**< Logging file descriptor */
+    int             timezone;               /**< Minutes west of Greenwich without DST */
 
     /*
         Service pointers
