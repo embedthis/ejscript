c19fdddc3e0c990c46f56d2f69d8231dfda39cc2
Embedthis Software <dev@embedthis.com>
2011-01-17 11:07:02 -0800

    Update deps

--- src/deps/mpr/mpr.h
+++ src/deps/mpr/mpr.h
@@ -5607,7 +5607,7 @@ extern void mprManageSelect(MprWaitService *ws, int flags);
 #endif
 
 #if BLD_WIN_LIKE
-extern int  mprInitWindow(MprWaitService *ws);
+extern int  mprInitWindow();
 extern void mprSetWinMsgCallback(MprWaitService *ws, MprMsgCallback callback);
 extern void mprServiceWinIO(MprWaitService *ws, int sockFd, int winMask);
 #endif
