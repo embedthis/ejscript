630df04536e663945a26f434bb83b2113f2d90c5
Embedthis Software <dev@embedthis.com>
2011-02-24 13:55:38 -0800

    Dont cache http error responses

--- src/deps/http/httpLib.c
+++ src/deps/http/httpLib.c
@@ -11614,6 +11614,7 @@ void httpSetResponseBody(HttpConn *conn, int status, cchar *msg)
         emsg = mprEscapeHtml(msg);
         httpFormatBody(conn, statusMsg, "<h2>Access Error: %d -- %s</h2>\r\n<p>%s</p>\r\n", status, statusMsg, emsg);
     }
+    httpDontCache(conn);
 }
 
 
