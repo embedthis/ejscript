058442e65eb36383786bf0db6062c262dc79b22b
Embedthis Software <dev@embedthis.com>
2011-04-28 13:30:51 -0700

    Update tools

--- src/deps/mpr/mprLib.c
+++ src/deps/mpr/mprLib.c
@@ -13012,6 +13012,7 @@ static char *probe(cchar *filename)
     }
     return 0;
 }
+#endif
 
 
 /*
@@ -13019,6 +13020,7 @@ static char *probe(cchar *filename)
  */
 char *mprSearchForModule(cchar *filename)
 {
+#if BLD_CC_DYN_LOAD
     char    *path, *f, *searchPath, *dir, *tok;
 
     filename = mprGetNormalizedPath(filename);
@@ -13045,9 +13047,9 @@ char *mprSearchForModule(cchar *filename)
         }
         dir = stok(0, MPR_SEARCH_SEP, &tok);
     }
+#endif /* BLD_CC_DYN_LOAD */
     return 0;
 }
-#endif
 
 
 /*
