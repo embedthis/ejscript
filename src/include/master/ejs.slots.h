68de52dd7c9a7ed386cb169b5ceb4fe625d76211
Embedthis Software <dev@embedthis.com>
2011-07-05 17:43:39 -1000

    Update slots

--- src/include/master/ejs.slots.h
+++ src/include/master/ejs.slots.h
@@ -2339,6 +2339,6 @@
 #define ES_XMLList_NUM_INSTANCE_PROP                                   20
 #define ES_XMLList_NUM_INHERITED_PROP                                  0
 
-#define _ES_CHECKSUM_ejs   1444478
+#define _ES_CHECKSUM_ejs   1444480
 
 #endif
