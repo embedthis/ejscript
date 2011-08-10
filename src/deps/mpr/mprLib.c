ac066025574de5ef1d54377eafd7bbb9eebec427
Embedthis Software <dev@embedthis.com>
2011-08-10 09:12:28 -0700

    Update deps

--- src/deps/mpr/mprLib.c
+++ src/deps/mpr/mprLib.c
@@ -14102,6 +14102,38 @@ int mprDeletePath(cchar *path)
 }
 
 
+static MprList *findFiles(MprList *list, cchar *dir, int flags)
+{
+    MprDirEntry     *dp;
+    MprList         *files;
+    int             next, enumDirs, incDirs;
+
+    enumDirs = flags & MPR_PATH_ENUM_DIRS ? 1 : 0;
+    incDirs = flags & MPR_PATH_INC_DIRS ? 1 : 0;
+
+    files = mprGetPathFiles(dir, enumDirs);
+    for (next = 0; (dp = mprGetNextItem(files, &next)) != 0; ) {
+        if (dp->isDir) {
+            if (incDirs) {
+                mprAddItem(list, mprJoinPath(dir, dp->name));
+            }
+            if (enumDirs) {
+                findFiles(list, mprJoinPath(dir, dp->name), flags);
+            } 
+        } else {
+            mprAddItem(list, mprJoinPath(dir, dp->name));
+        }
+    }
+    return list;
+}
+
+
+MprList *mprFindFiles(cchar *dir, int flags)
+{
+    return findFiles(mprCreateList(-1, 0), dir, flags);
+}
+
+
 /*
     Return an absolute (normalized) path.
  */
@@ -14249,7 +14281,7 @@ char *mprGetAppPath()
  */
 char *mprGetCurrentPath()
 {
-    char            dir[MPR_MAX_PATH];
+    char    dir[MPR_MAX_PATH];
 
     if (getcwd(dir, sizeof(dir)) == 0) {
         return mprGetAbsPath("/");
@@ -14296,6 +14328,9 @@ char *mprGetPathBase(cchar *path)
     MprFileSystem   *fs;
     char            *cp;
 
+    if (path == 0) {
+        return sclone("");
+    }
     fs = mprLookupFileSystem(path);
     cp = (char*) lastSep(fs, path);
     if (cp == 0) {
@@ -15379,7 +15414,7 @@ char *mprSearchPath(cchar *file, int flags, cchar *search, ...)
 }
 
 
-int mprWritePath(cchar *path, cchar *buf, ssize len, int mode)
+ssize mprWritePath(cchar *path, cchar *buf, ssize len, int mode)
 {
     MprFile     *file;
 
@@ -15395,7 +15430,7 @@ int mprWritePath(cchar *path, cchar *buf, ssize len, int mode)
         return MPR_ERR_CANT_WRITE;
     }
     mprCloseFile(file);
-    return 0;
+    return len;
 }
 
 
@@ -20158,10 +20193,10 @@ char *srejoinv(char *buf, va_list args)
 }
 
 
-char *sreplace(char *str, char *pattern, char *replacement)
+char *sreplace(cchar *str, cchar *pattern, cchar *replacement)
 {
     MprBuf      *buf;
-    char        *s;
+    cchar       *s;
     ssize       plen;
 
     buf = mprCreateBuf(-1, -1);
