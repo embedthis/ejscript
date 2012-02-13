62fa4e849339f15b6e641666bbd44ebdab514f5b
Embedthis Software <dev@embedthis.com>
2012-02-12 22:00:26 -0800

    Support null as require arg

--- src/cmd/ejsc.c
+++ src/cmd/ejsc.c
@@ -160,12 +160,10 @@ MAIN(ejscMain, int argc, char **argv, char **envp)
                     app->modules = mprCreateList(-1, 0);
                 }
                 modules = sclone(argv[++nextArg]);
-#if MACOSX || WIN
                 /*  Fix for Xcode and Visual Studio */
                 if (modules[0] == ' ' || scmp(modules, "null") == 0) {
                     modules[0] = '\0';                    
                 }
-#endif
                 name = stok(modules, " \t,", &tok);
                 while (name != NULL) {
                     require(name);
