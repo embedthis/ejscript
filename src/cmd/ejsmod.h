f01b67dc5213910c739437740718f6bc550d1938
Embedthis Software <dev@embedthis.com>
2012-10-29 18:19:16 -0700

    Rename mprAssert to assure

--- src/cmd/ejsmod.h
+++ src/cmd/ejsmod.h
@@ -113,7 +113,7 @@ extern int  emCreateDoc(EjsMod *mp);
     under the terms of the GNU General Public License as published by the 
     Free Software Foundation; either version 2 of the License, or (at your 
     option) any later version. See the GNU General Public License for more 
-    details at: http://www.embedthis.com/downloads/gplLicense.html
+    details at: http://embedthis.com/downloads/gplLicense.html
     
     This program is distributed WITHOUT ANY WARRANTY; without even the 
     implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
@@ -122,7 +122,7 @@ extern int  emCreateDoc(EjsMod *mp);
     proprietary programs. If you are unable to comply with the GPL, you must
     acquire a commercial license to use this software. Commercial licenses 
     for this software and support services are available from Embedthis 
-    Software at http://www.embedthis.com 
+    Software at http://embedthis.com 
     
     Local variables:
     tab-width: 4
