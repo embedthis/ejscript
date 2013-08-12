5e49afa36b9abd8d917b4e9e230999f8f50290c6
Embedthis Software <dev@embedthis.com>
2013-08-12 16:59:17 -0700

    Run main dispatcher if using events thread

--- src/deps/mpr/mprLib.c
+++ src/deps/mpr/mprLib.c
@@ -2452,6 +2452,7 @@ PUBLIC Mpr *mprCreate(int argc, char **argv, int flags)
         if (!(flags & MPR_NO_WINDOW)) {
             mprInitWindow();
         }
+        mprRunDispatcher(mpr->dispatcher);
     } else {
         mprStartEventsThread();
     }
@@ -8995,6 +8996,21 @@ PUBLIC void mprRelayEvent(MprDispatcher *dispatcher, void *proc, void *data, Mpr
 
     dequeueDispatcher(dispatcher);
     mprScheduleDispatcher(dispatcher);
+}
+
+
+/*
+    Internal use only. Run the "main" dispatcher if not using a user events thread. 
+ */
+PUBLIC int mprRunDispatcher(MprDispatcher *dispatcher)
+{
+    assert(dispatcher);
+    if (isRunning(dispatcher) && dispatcher->owner != mprGetCurrentOsThread()) {
+        mprError("Relay to a running dispatcher owned by another thread");
+        return MPR_ERR_BAD_STATE;
+    }
+    queueDispatcher(dispatcher->service->runQ, dispatcher);
+    return 0;
 }
 
 
