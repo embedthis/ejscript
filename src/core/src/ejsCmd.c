0e113b13c839c19457d9092dd361d4597029df6d
Embedthis Software <dev@embedthis.com>
2015-01-27 08:47:30 -0800

    FIX: missing readable event on Cmd child death.

--- src/core/src/ejsCmd.c
+++ src/core/src/ejsCmd.c
@@ -289,6 +289,9 @@ static void cmdIOCallback(MprCmd *mc, int channel, void *data)
         break;
     default:
         /* Child death */
+        if (cmd->async) {
+            ejsSendEvent(cmd->ejs, cmd->emitter, "readable", NULL, cmd);
+        }
         return;
     }
     /*
