c77d9cc0cc52442700a916b0770e07cb68f46df8
Embedthis Software <dev@embedthis.com>
2015-06-05 11:47:05 -0700

    REGEN: projects

--- projects/ejs-linux-default.mk
+++ projects/ejs-linux-default.mk
@@ -2117,7 +2117,6 @@ ifeq ($(ME_COM_SSL),1)
 #
 DEPS_139 += src/certs/samples/ca.crt
 DEPS_139 += src/certs/samples/ca.key
-DEPS_139 += src/certs/samples/dh.pem
 DEPS_139 += src/certs/samples/ec.crt
 DEPS_139 += src/certs/samples/ec.key
 DEPS_139 += src/certs/samples/roots.crt
@@ -2131,7 +2130,6 @@ $(BUILD)/.install-certs-modified: $(DEPS_139)
 	mkdir -p "$(BUILD)/bin"
 	cp src/certs/samples/ca.crt $(BUILD)/bin/ca.crt
 	cp src/certs/samples/ca.key $(BUILD)/bin/ca.key
-	cp src/certs/samples/dh.pem $(BUILD)/bin/dh.pem
 	cp src/certs/samples/ec.crt $(BUILD)/bin/ec.crt
 	cp src/certs/samples/ec.key $(BUILD)/bin/ec.key
 	cp src/certs/samples/roots.crt $(BUILD)/bin/roots.crt
--- projects/ejs-vxworks-default.mk
+++ projects/ejs-vxworks-default.mk
@@ -2004,7 +2004,6 @@ ifeq ($(ME_COM_SSL),1)
 #
 DEPS_138 += src/certs/samples/ca.crt
 DEPS_138 += src/certs/samples/ca.key
-DEPS_138 += src/certs/samples/dh.pem
 DEPS_138 += src/certs/samples/ec.crt
 DEPS_138 += src/certs/samples/ec.key
 DEPS_138 += src/certs/samples/roots.crt
@@ -2018,7 +2017,6 @@ $(BUILD)/.install-certs-modified: $(DEPS_138)
 	mkdir -p "$(BUILD)/bin"
 	cp src/certs/samples/ca.crt $(BUILD)/bin/ca.crt
 	cp src/certs/samples/ca.key $(BUILD)/bin/ca.key
-	cp src/certs/samples/dh.pem $(BUILD)/bin/dh.pem
 	cp src/certs/samples/ec.crt $(BUILD)/bin/ec.crt
 	cp src/certs/samples/ec.key $(BUILD)/bin/ec.key
 	cp src/certs/samples/roots.crt $(BUILD)/bin/roots.crt
